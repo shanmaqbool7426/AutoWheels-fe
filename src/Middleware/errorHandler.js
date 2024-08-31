1f48e2eefe5348547657f83f6a7c187e9d7994b1
shanmaqbool <shanmaqbool12345@gmail.com>
2024-08-31 00:36:26 -0700

    restor beckup

--- src/Middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-import responses from "../Utils/response.js";
-
-function errorHandler(err, req, res, next) {
-    console.error(err.stack);
-    responses.internalServerError(res, 'Internal Server Error');
-  }
-  
- export { errorHandler};
-  
--- src/Utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, 'token-secret-scv', {
-    expiresIn: '30d',
-  });
-};
-
-export default generateToken;
