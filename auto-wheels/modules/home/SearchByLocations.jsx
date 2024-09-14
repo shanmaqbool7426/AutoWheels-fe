2fec67c1593e387a60b0e6abac01f2b861a50c74
wahabansari <wahab0421690@gmail.com>
2024-09-14 21:49:18 +0500

    imporve UI

--- auto-wheels/modules/home/SearchByLocations.jsx
+++ auto-wheels/modules/home/SearchByLocations.jsx
@@ -1,4 +1,16 @@
-import Image from "next/image";
+"use client";
+import {
+  Anchor,
+  Flex,
+  Input,
+  SimpleGrid,
+  Text,
+  Title,
+  Image,
+  Card,
+  Grid,
+} from "@mantine/core";
+import NextImage from "next/image";
 import Link from "next/link";
 import React from "react";
 import { FaLocationDot } from "react-icons/fa6";
@@ -9,246 +21,285 @@ const SearchByLocations = () => {
       <div className="container">
         <div className="row">
           <div className="col-12">
-            <div className="category-title d-flex justify-content-between align-items-center">
-              <h3 className="fw-bold mb-0">
-                Get trusted used Cars
-                <span className="text-primary ms-1">Nearby</span>
-              </h3>
-            </div>
+            <Flex justify="space-between" align="center">
+              <Title order={2}>
+                Get trusted used Cars{" "}
+                <Text span c="#E90808" inherit>
+                  Nearby
+                </Text>
+              </Title>
+
+              <Anchor component={Link} href="#" c="#E90808">
+                Show all Makes
+              </Anchor>
+            </Flex>
           </div>
         </div>
+
         <div className="row mt-5">
           <div className="col-lg-8">
-            <div className="row">
+            <div className="row g-3">
               <div className="col">
-                <div className="card locations-card">
-                  <div className="card-body text-center">
-                    <Link href={"#"} className="text-decoration-none">
-                      <Image
-                        src={"/locations/karachi.svg"}
-                        width={80}
-                        height={95}
-                        alt="Karachi"
-                      />
-                      <h5 className="mb-0 my-3 location-info">
-                        <span className="text-muted d-block">Used cars in</span>
-                        <span className="fw-semibold mt-1 d-inline-block text-dark city-name">
-                          Karachi
-                        </span>
-                      </h5>
-                    </Link>
-                  </div>
-                </div>
+                <Card
+                  mb="md"
+                  className="align-items-center"
+                  component={Anchor}
+                  td="none"
+                  padding="md"
+                  shadow="0px 4px 20px 0px rgba(0, 0, 0, 0.0784313725)"
+                >
+                  <NextImage
+                    src="/locations/karachi.svg"
+                    width={80}
+                    height={95}
+                    alt="Karachi"
+                  />
+                  <Text size="sm" c="dimmed">
+                    {" "}
+                    Used cars in
+                  </Text>
+                  <Title fw={600} order={6}>
+                    Karachi
+                  </Title>
+                </Card>
               </div>
               <div className="col">
-                <div className="card locations-card">
-                  <div className="card-body text-center">
-                    <Link href={"#"} className="text-decoration-none">
-                      <Image
-                        src={"/locations/islamabad.svg"}
-                        width={80}
-                        height={95}
-                        alt="Islamabad"
-                      />
-                      <h5 className="mb-0 my-3 location-info">
-                        <span className="text-muted d-block">Used cars in</span>
-                        <span className="fw-semibold mt-1 d-inline-block text-dark city-name">
-                          Islamabad
-                        </span>
-                      </h5>
-                    </Link>
-                  </div>
-                </div>
+                <Card
+                  mb="md"
+                  className="align-items-center"
+                  component={Anchor}
+                  td="none"
+                  padding="md"
+                  shadow="0px 4px 20px 0px rgba(0, 0, 0, 0.0784313725)"
+                >
+                  <NextImage
+                    src={"/locations/islamabad.svg"}
+                    width={80}
+                    height={95}
+                    alt="Islamabad"
+                  />
+                  <Text size="sm" c="dimmed">
+                    {" "}
+                    Used cars in
+                  </Text>
+                  <Title fw={600} order={6}>
+                    Islamabad
+                  </Title>
+                </Card>
               </div>
               <div className="col">
-                <div className="card locations-card">
-                  <div className="card-body text-center">
-                    <Link href={"#"} className="text-decoration-none">
-                      <Image
-                        src={"/locations/lahore.svg"}
-                        width={80}
-                        height={95}
-                        alt="Lahore"
-                      />
-                      <h5 className="mb-0 my-3 location-info">
-                        <span className="text-muted d-block">Used cars in</span>
-                        <span className="fw-semibold mt-1 d-inline-block text-dark city-name">
-                          Lahore
-                        </span>
-                      </h5>
-                    </Link>
-                  </div>
-                </div>
+                <Card
+                  mb="md"
+                  className="align-items-center"
+                  component={Anchor}
+                  td="none"
+                  padding="md"
+                  shadow="0px 4px 20px 0px rgba(0, 0, 0, 0.0784313725)"
+                >
+                  <NextImage
+                    src={"/locations/lahore.svg"}
+                    width={80}
+                    height={95}
+                    alt="Lahore"
+                  />
+                  <Text size="sm" c="dimmed">
+                    {" "}
+                    Used cars in
+                  </Text>
+                  <Title fw={600} order={6}>
+                    Lahore
+                  </Title>
+                </Card>
               </div>
               <div className="col">
-                <div className="card locations-card">
-                  <div className="card-body text-center">
-                    <Link href={"#"} className="text-decoration-none">
-                      <Image
-                        src={"/locations/faisalabad.svg"}
-                        width={80}
-                        height={95}
-                        alt="Faisalabad"
-                      />
-                      <h5 className="mb-0 my-3 location-info">
-                        <span className="text-muted d-block">Used cars in</span>
-                        <span className="fw-semibold mt-1 d-inline-block text-dark city-name">
-                          Faisalabad
-                        </span>
-                      </h5>
-                    </Link>
-                  </div>
-                </div>
+                <Card
+                  mb="md"
+                  className="align-items-center"
+                  component={Anchor}
+                  td="none"
+                  padding="md"
+                  shadow="0px 4px 20px 0px rgba(0, 0, 0, 0.0784313725)"
+                >
+                  <NextImage
+                    src={"/locations/faisalabad.svg"}
+                    width={80}
+                    height={95}
+                    alt="Faisalabad"
+                  />
+                  <Text size="sm" c="dimmed">
+                    {" "}
+                    Used cars in
+                  </Text>
+                  <Title fw={600} order={6}>
+                    Faisalabad
+                  </Title>
+                </Card>
               </div>
               <div className="col">
-                <div className="card locations-card">
-                  <div className="card-body text-center">
-                    <Link href={"#"} className="text-decoration-none">
-                      <Image
-                        src={"/locations/peshawar.svg"}
-                        width={80}
-                        height={95}
-                        alt="Peshawar"
-                      />
-                      <h5 className="mb-0 my-3 location-info">
-                        <span className="text-muted d-block">Used cars in</span>
-                        <span className="fw-semibold mt-1 d-inline-block text-dark city-name">
-                          Peshawar
-                        </span>
-                      </h5>
-                    </Link>
-                  </div>
-                </div>
+                <Card
+                  mb="md"
+                  className="align-items-center"
+                  component={Anchor}
+                  td="none"
+                  padding="md"
+                  shadow="0px 4px 20px 0px rgba(0, 0, 0, 0.0784313725)"
+                >
+                  <NextImage
+                    src={"/locations/peshawar.svg"}
+                    width={80}
+                    height={95}
+                    alt="Peshawar"
+                  />
+                  <Text size="sm" c="dimmed">
+                    {" "}
+                    Used cars in
+                  </Text>
+                  <Title fw={600} order={6}>
+                    Peshawar
+                  </Title>
+                </Card>
               </div>
             </div>
-            <div className="row">
+            <div className="row g-3">
               <div className="col">
-                <div className="card locations-card">
-                  <div className="card-body text-center">
-                    <Link href={"#"} className="text-decoration-none">
-                      <Image
-                        src={"/locations/karachi.svg"}
-                        width={80}
-                        height={95}
-                        alt="Karachi"
-                      />
-                      <h5 className="mb-0 my-3 location-info">
-                        <span className="text-muted d-block">Used cars in</span>
-                        <span className="fw-semibold mt-1 d-inline-block text-dark city-name">
-                          Karachi
-                        </span>
-                      </h5>
-                    </Link>
-                  </div>
-                </div>
+                <Card
+                  mb="md"
+                  className="align-items-center"
+                  component={Anchor}
+                  td="none"
+                  padding="md"
+                  shadow="0px 4px 20px 0px rgba(0, 0, 0, 0.0784313725)"
+                >
+                  <NextImage
+                    src="/locations/karachi.svg"
+                    width={80}
+                    height={95}
+                    alt="Karachi"
+                  />
+                  <Text size="sm" c="dimmed">
+                    {" "}
+                    Used cars in
+                  </Text>
+                  <Title fw={600} order={6}>
+                    Karachi
+                  </Title>
+                </Card>
               </div>
               <div className="col">
-                <div className="card locations-card">
-                  <div className="card-body text-center">
-                    <Link href={"#"} className="text-decoration-none">
-                      <Image
-                        src={"/locations/islamabad.svg"}
-                        width={80}
-                        height={95}
-                        alt="Islamabad"
-                      />
-                      <h5 className="mb-0 my-3 location-info">
-                        <span className="text-muted d-block">Used cars in</span>
-                        <span className="fw-semibold mt-1 d-inline-block text-dark city-name">
-                          Islamabad
-                        </span>
-                      </h5>
-                    </Link>
-                  </div>
-                </div>
+                <Card
+                  mb="md"
+                  className="align-items-center"
+                  component={Anchor}
+                  td="none"
+                  padding="md"
+                  shadow="0px 4px 20px 0px rgba(0, 0, 0, 0.0784313725)"
+                >
+                  <NextImage
+                    src={"/locations/islamabad.svg"}
+                    width={80}
+                    height={95}
+                    alt="Islamabad"
+                  />
+                  <Text size="sm" c="dimmed">
+                    {" "}
+                    Used cars in
+                  </Text>
+                  <Title fw={600} order={6}>
+                    Islamabad
+                  </Title>
+                </Card>
               </div>
               <div className="col">
-                <div className="card locations-card">
-                  <div className="card-body text-center">
-                    <Link href={"#"} className="text-decoration-none">
-                      <Image
-                        src={"/locations/lahore.svg"}
-                        width={80}
-                        height={95}
-                        alt="Lahore"
-                      />
-                      <h5 className="mb-0 my-3 location-info">
-                        <span className="text-muted d-block">Used cars in</span>
-                        <span className="fw-semibold mt-1 d-inline-block text-dark city-name">
-                          Lahore
-                        </span>
-                      </h5>
-                    </Link>
-                  </div>
-                </div>
+                <Card
+                  mb="md"
+                  className="align-items-center"
+                  component={Anchor}
+                  td="none"
+                  padding="md"
+                  shadow="0px 4px 20px 0px rgba(0, 0, 0, 0.0784313725)"
+                >
+                  <NextImage
+                    src={"/locations/lahore.svg"}
+                    width={80}
+                    height={95}
+                    alt="Lahore"
+                  />
+                  <Text size="sm" c="dimmed">
+                    {" "}
+                    Used cars in
+                  </Text>
+                  <Title fw={600} order={6}>
+                    Lahore
+                  </Title>
+                </Card>
               </div>
               <div className="col">
-                <div className="card locations-card">
-                  <div className="card-body text-center">
-                    <Link href={"#"} className="text-decoration-none">
-                      <Image
-                        src={"/locations/faisalabad.svg"}
-                        width={80}
-                        height={95}
-                        alt="Faisalabad"
-                      />
-                      <h5 className="mb-0 my-3 location-info">
-                        <span className="text-muted d-block">Used cars in</span>
-                        <span className="fw-semibold mt-1 d-inline-block text-dark city-name">
-                          Faisalabad
-                        </span>
-                      </h5>
-                    </Link>
-                  </div>
-                </div>
+                <Card
+                  mb="md"
+                  className="align-items-center"
+                  component={Anchor}
+                  td="none"
+                  padding="md"
+                  shadow="0px 4px 20px 0px rgba(0, 0, 0, 0.0784313725)"
+                >
+                  <NextImage
+                    src={"/locations/faisalabad.svg"}
+                    width={80}
+                    height={95}
+                    alt="Faisalabad"
+                  />
+                  <Text size="sm" c="dimmed">
+                    {" "}
+                    Used cars in
+                  </Text>
+                  <Title fw={600} order={6}>
+                    Faisalabad
+                  </Title>
+                </Card>
               </div>
               <div className="col">
-                <div className="card locations-card">
-                  <div className="card-body text-center">
-                    <Link href={"#"} className="text-decoration-none">
-                      <Image
-                        src={"/locations/peshawar.svg"}
-                        width={80}
-                        height={95}
-                        alt="Peshawar"
-                      />
-                      <h5 className="mb-0 my-3 location-info">
-                        <span className="text-muted d-block">Used cars in</span>
-                        <span className="fw-semibold mt-1 d-inline-block text-dark city-name">
-                          Peshawar
-                        </span>
-                      </h5>
-                    </Link>
-                  </div>
-                </div>
+                <Card
+                  mb="md"
+                  className="align-items-center"
+                  component={Anchor}
+                  td="none"
+                  padding="md"
+                  shadow="0px 4px 20px 0px rgba(0, 0, 0, 0.0784313725)"
+                >
+                  <NextImage
+                    src={"/locations/peshawar.svg"}
+                    width={80}
+                    height={95}
+                    alt="Peshawar"
+                  />
+                  <Text size="sm" c="dimmed">
+                    {" "}
+                    Used cars in
+                  </Text>
+                  <Title fw={600} order={6}>
+                    Peshawar
+                  </Title>
+                </Card>
               </div>
             </div>
           </div>
-          <div className="col-lg-4">
-            <div className="position-relative h-100 ms-5">
-              <h5 className="lh-base text-center input-label py-4">
-                I am looking to buy a second <br /> hand car in
-              </h5>
-              <div className="input-with-icon">
-                <span className="icon">
-                  <FaLocationDot />
-                </span>
-                <input
-                  type="text"
-                  placeholder="Enter your city"
-                  className="location-input form-control"
-                />
-              </div>
-              <div className="locations-background">
-                <Image
-                  src={"/locations/locations-bg.svg"}
-                  width={500}
-                  height={500}
-                  className="img-fluid"
-                  alt="Location Vector Background"
-                />
-              </div>
-            </div>
+          <div className="col-lg-4 d-none d-lg-block position-relative">
+            <Title c="dimmed" fw={400} ta="center" order={4} my="xl">
+              I am looking to buy a second <br /> hand car in
+            </Title>
+            <Input
+              leftSection={<FaLocationDot />}
+              placeholder="Enter your city"
+              size="md"
+              mx="xl"
+            />
+            <Image
+              pos="absolute"
+              bottom={0}
+              src={"/locations/locations-bg.svg"}
+              className="img-fluid"
+              alt="Location Vector Background"
+            />
           </div>
         </div>
       </div>
