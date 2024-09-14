302dc505563e29128f901afba3ec2d609728d6cc
shanmaqbool <shanmaqbool12345@gmail.com>
2024-09-14 05:45:37 -0700

    adding issues

--- auto-wheels/services/fetchAPI.js
+++ auto-wheels/services/fetchAPI.js
@@ -14,7 +14,7 @@ export const fetchAPI = async (url, options = {}) => {
 
   try {
     // Perform the fetch request
-    const response = await fetch(url, { revalidate: 3600, cache: 'no-store' });
+    const response = await fetch(url);
 
     // Check if the response is ok (status in the range 200-299)
     if (!response.ok) {
